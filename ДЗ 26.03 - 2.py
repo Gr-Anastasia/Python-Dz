872a73ab533db26f7a4a109cb27707328aa2db27
Nast <Nast@123.ru>
2025-03-29 14:34:35 +0500

    Дз по питону 26.03

---
+++ ДЗ 26.03 - 2.py
@@ -0,0 +1,39 @@
+C = int(input("Сколько чисел будем складывать?: "))
+a = 0
+
+
+for x in range (C):
+    y = input("Введите число: ")
+    if y == "stop":
+        print(a)
+        break
+    else:
+        y = int(y)
+        a += y
+
+
+
+
+    
+
+    
+
+    
+    
+
+
+
+     
+
+   
+
+
+    
+
+
+
+
+   
+    
+
+
